8fb056b5e9ab5e891b85dbe26d9493b40353a5f0
Victor Tinus <vatt849@hotmail.com>
2023-09-09 21:10:39 +0300

    fill pages layouts

---
+++ anilibria-winui/Models/Release.cs
@@ -0,0 +1,9 @@
+﻿namespace anilibria.Models
+{
+    public class Release
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageLocation { get; set; }
+    }
+}
--- anilibria-winui/Pages/ReleasesPage.xaml.cs
+++ anilibria-winui/Pages/ReleasesPage.xaml.cs
@@ -1,4 +1,8 @@
+using anilibria.Models;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,9 +14,31 @@ namespace anilibria.Pages
     /// </summary>
     public sealed partial class ReleasesPage : Page
     {
+        public List<Release> staticReleaseData;
+
         public ReleasesPage()
         {
             InitializeComponent();
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            staticReleaseData = new();
+
+            var rnd = new Random();
+            List<Release> tempList = new List<Release>(
+                                      Enumerable.Range(0, 1000).Select(k =>
+                                        new Release
+                                        {
+                                            Title = "Title " + k.ToString(),
+                                            ImageLocation = @"F:\UserData\Pictures\5c53eb75-a102-427b-9451-28ade1f7ddd1.png"
+                                        }));
+
+            foreach (Release r in tempList)
+            {
+                staticReleaseData.Add(r);
+            }
         }
     }
 }
